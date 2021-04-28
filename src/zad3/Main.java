45f0fe3e0719546705b42af5f6a43bb4e7b4e28a
Krzysztof Kaczyński <qris70@gmail.com>
2021-04-28 20:15:40 +0200

    add Zad3

--- src/zad3/Main.java
+++ src/zad3/Main.java
@@ -5,10 +5,37 @@
 package zad3;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         String fname = System.getProperty("user.home") + "/pakiety.txt";
+        try {
+            Scanner scan = new Scanner(new File(fname));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
+
+        int megabit = 1048576;
+
+        int sizeOfFirstPackage = 1;
+        int odczytane = 0;
+        int freeSpace = odczytane * megabit;
+        int sizeOfNextPackage = sizeOfFirstPackage;
+        int usedSpace = 0;
+        int nr = 0;
+        while (usedSpace + sizeOfNextPackage <= freeSpace) {
+            usedSpace += sizeOfNextPackage;
+            if (nr < 4) {
+                sizeOfNextPackage *= 2;
+            } else {
+                sizeOfNextPackage *= 3;
+            }
+            nr++;
+        }
     }
 }
