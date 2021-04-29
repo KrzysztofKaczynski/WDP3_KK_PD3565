e0ae2ddc364042001381bea2d2084bbf5bf748de
Krzysztof <qris70@gmail.com>
2021-04-29 09:15:33 +0200

    add zad3 poprawione

--- src/zad3/Main.java
+++ src/zad3/Main.java
@@ -5,37 +5,80 @@
 package zad3;
 
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) {
         String fname = System.getProperty("user.home") + "/pakiety.txt";
+        int rozmiarPakietu;
+
+        int pojemnoscHd;
+
+        int pojemnoscHdWBajtach;
+
+        int sumaPak = 0;
+
+        ArrayList<Integer> rozmPakList = new ArrayList<>();
+
+        int licznikPak = 1;
+
         try {
-            Scanner scan = new Scanner(new File(fname));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+            File file = new File(fname);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String wierszZmiennych = bufferedReader.readLine();
+            StringTokenizer tokenize = new StringTokenizer(wierszZmiennych, " ");
+
+            rozmiarPakietu = Integer.parseInt(tokenize.nextToken());
+            pojemnoscHd = Integer.parseInt(tokenize.nextToken());
+
+            pojemnoscHdWBajtach = pojemnoscHd * 1024 * 1024;
 
 
-        int megabit = 1048576;
+            if (rozmiarPakietu < 0 || pojemnoscHd < 0)
+                throw new Exception();
 
-        int sizeOfFirstPackage = 1;
-        int odczytane = 0;
-        int freeSpace = odczytane * megabit;
-        int sizeOfNextPackage = sizeOfFirstPackage;
-        int usedSpace = 0;
-        int nr = 0;
-        while (usedSpace + sizeOfNextPackage <= freeSpace) {
-            usedSpace += sizeOfNextPackage;
-            if (nr < 4) {
-                sizeOfNextPackage *= 2;
-            } else {
-                sizeOfNextPackage *= 3;
+
+            while (sumaPak < pojemnoscHdWBajtach) {
+                if (licznikPak == 1) {
+                    rozmPakList.add(rozmiarPakietu);
+                    sumaPak += rozmiarPakietu;
+                }
+                if (licznikPak > 1 && licznikPak < 6) {
+                    rozmPakList.add(rozmPakList.get(rozmPakList.size() - 1) * 2);
+                    sumaPak += rozmPakList.get(rozmPakList.size() - 1);
+                }
+                if (licznikPak >= 6) {
+                    rozmPakList.add(rozmPakList.get(rozmPakList.size() - 1) * 3);
+                    sumaPak += rozmPakList.get(rozmPakList.size() - 1);
+                }
+                licznikPak++;
             }
-            nr++;
+
+            sumaPak -= rozmPakList.get(rozmPakList.size() - 1);
+            rozmPakList.remove(rozmPakList.size() - 1);
+
+
+            System.out.println(rozmPakList.size());
+
+
+            int i = 1;
+            for (int list : rozmPakList) {
+                System.out.println(i + " " + list);
+                i++;
+            }
+
+            // Wyświetlenie sumy pakietów
+            System.out.println(sumaPak);
+
+        } catch (Exception e) {
+            System.out.println("***");
+            System.exit(1);
         }
     }
 }
