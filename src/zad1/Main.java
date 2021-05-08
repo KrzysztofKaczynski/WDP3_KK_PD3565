87528417e5af5d9d137da6a84f52688c32b2f3af
Krzysztof <qris70@gmail.com>
2021-05-08 08:20:48 +0200

    add Exception exc

--- src/zad1/Main.java
+++ src/zad1/Main.java
@@ -33,7 +33,7 @@ public class Main {
 
                 System.out.println(suma);
             }
-        } catch (FileNotFoundException exc) {
+        } catch (Exception exc) {
             System.out.println("***");
             System.exit(0);
         }
